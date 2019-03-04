df1b5fac19ecc1c4c51272cc2572ca66ca2c1f5b
Carlos M Garcia <cgarcia101015@gmail.com>
2019-03-04 16:32:38 -0500

    added shuffle, and score functionality

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,28 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Nav from './components/Navbar';
+import Header from './components/Header';
+import Main from './components/Main';
+import rickMorty from './components/rickMorty.json';
+import Footer from './components/Footer';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	// Setting this.state.rickMorty
+
+	state = {
+		rickMorty,
+		clickedImage: [],
+		wins: 0,
+		loss: 0
+	};
+
+	shuffleRick = () => {
+		const rickMorty = this.state.rickMorty;
+
+		for (let i = 0; i < rickMorty.length; i++) {
+			const randomIndex = Math.floor(Math.random() * rickMorty.length);
+			console.log(randomIndex);
+			const tempImg = rickMorty[i];
+			rickMorty[i] = rickMorty[randomIndex];
+			rickMorty[randomIndex] = tempImg;
+		}
+		return rickMorty;
+	};
+
+	handleClick = (id) => {
+		const clickedImage = this.state.clickedImage;
+		let wins = this.state.wins;
+		let loss = this.state.loss;
+		if (clickedImage.includes(id)) {
+			console.log('You Lost!');
+			loss++;
+			const randomArray = this.shuffleRick();
+			this.setState({ clickedImage, rickMorty: randomArray, loss });
+		} else {
+			wins++;
+			clickedImage.push(id);
+			const randomArray = this.shuffleRick();
+			this.setState({ clickedImage, rickMorty: randomArray, wins });
+		}
+	};
+
+	render() {
+		return (
+			<div>
+				<Nav />
+				<Header wins={this.state.wins} loss={this.state.loss} />
+				{this.state.rickMorty.map((rickMorty) => (
+					<Main id={rickMorty.id} key={rickMorty.id} image={rickMorty.image} handleClick={this.handleClick} />
+				))}
+				<Footer />
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ clickygame/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+function Footer() {
+	return (
+		<div className="footer">
+			<span>ClickyGame App ©</span>
+		</div>
+	);
+}
+
+export default Footer;
